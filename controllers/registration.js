63e3994df5a725ebb55d685c07948dc70d953d5a
CodeWUsama <khawajausama775@gmail.com>
2023-07-04 16:41:17 +0500

    case insensitive clause

--- controllers/registration.js
+++ controllers/registration.js
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import sendResponse from '../helpers/responseHelper.js';
 import User from '../models/users.js';
 import { userRegistrationSchema } from '../schemas/users.js';
@@ -5,7 +6,13 @@ import { userRegistrationSchema } from '../schemas/users.js';
 const handleSignup = async (req, res) => {
   await userRegistrationSchema.validate(req.body);
   const { fullname, email, password } = req.body;
-  const user = await User.findOne({ where: { email } });
+  const user = await User.findOne({
+    where: {
+      email: {
+        [Op.iLike]: email,
+      },
+    },
+  });
   if (user) {
     return sendResponse(res, null, 'User already exists');
   }
